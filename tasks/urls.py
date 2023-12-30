1e292f599f034d1320292cceb3c7bda6b8049269
Rahil Vithalani <rvithalani.rjv@gmail.com>
2023-12-30 12:18:08 +0530

    basic functionality done

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,7 +6,7 @@ from .views import *
 urlpatterns = [
     path('<int:pk>/', DetailTask.as_view()),
     path('list', ListTask.as_view()),
-    path('create', CreateTask.as_view()),
-    path('delete/<int:pk>', DeleteTask.as_view()),
+    path('create', CreateTask.as_view(), name='task-create'),
+    path('delete/<int:pk>/', DeleteTask.as_view(), name='delete-task'),
     path('', TaskManagerView.as_view(), name='task-manager')
 ]
