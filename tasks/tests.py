40dfb1a1ef2ae8df9ce765ca720809b1ca8c5a2e
Rahil Vithalani <rvithalani.rjv@gmail.com>
2023-12-30 12:27:41 +0530

    added unit test case baisc

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+# tasks/tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Task
+
+class TaskAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.task_data = {'title': 'Test Task', 'description': 'Test Description'}
+
+    def test_create_task(self):
+        url = reverse('task-create')
+        response = self.client.post(url, self.task_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.get().title, 'Test Task')
+
+    def test_list_tasks(self):
+        Task.objects.create(title='Test Task', description='Test Description')
+        url = reverse('task-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(len(response.data), 0)
+
+    def test_retrieve_task(self):
+        task = Task.objects.create(title='Test Task', description='Test Description')
+        url = reverse('task-detail', args=[task.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Test Task')
+
+    def test_update_task(self):
+        task = Task.objects.create(title='Test Task', description='Test Description')
+        updated_data = {'title': 'Updated Task', 'description': 'Updated Description'}
+        url = reverse('task-detail', args=[task.id])
+        response = self.client.put(url, updated_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Task.objects.get(id=task.id).title, 'Updated Task')
+
+    def test_delete_task(self):
+        task = Task.objects.create(title='Test Task', description='Test Description')
+        url = reverse('delete-task', args=[task.id])
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Task.objects.count(), 0)
--- tasks/urls.py
+++ tasks/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<int:pk>/', DetailTask.as_view()),
-    path('list', ListTask.as_view()),
+    path('<int:pk>/', DetailTask.as_view(), name='task-detail'),
+    path('list', ListTask.as_view(), name='task-list'),
     path('create', CreateTask.as_view(), name='task-create'),
     path('delete/<int:pk>/', DeleteTask.as_view(), name='delete-task'),
     path('', TaskManagerView.as_view(), name='task-manager')
